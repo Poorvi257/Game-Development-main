b2a825fa8cd8c550aef8de8b7eac3b069df11e54
Poorvi Shrivastava <poorvi257s@gmail.com>
2024-02-24 21:34:38 -0500

    by default, come to login page

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
 const PORT = 8000
